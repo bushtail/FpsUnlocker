c76ac8536f17cfb4e975dc3b4a8d50e10ec8562b
bushtail <evannosich@icloud.com>
2025-03-24 16:13:22 -0400

    3.11

--- src/mod.ts
+++ src/mod.ts
@@ -1,25 +1,37 @@
-import { DependencyContainer } from "tsyringe";
+﻿import { DependencyContainer } from "tsyringe";
 
-import { IPostAkiLoadMod } from "@spt-aki/models/external/IPostAkiLoadMod";
-import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
-import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
-import { ConfigServer } from "@spt-aki/servers/ConfigServer";
-import { ConfigTypes } from "@spt-aki/models/enums/ConfigTypes";
-import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
-import { LogTextColor } from "@spt-aki/models/spt/logging/LogTextColor";
+import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
+import { ILogger } from "@spt/models/spt/utils/ILogger";
+import { DatabaseServer } from "@spt/servers/DatabaseServer";
 
 class FpsUnlocker implements IPostDBLoadMod
 {
     private modConfig = require("../config/config.json");
+    private isFikaInstalled: boolean = false;
+    private modName = "[FpsUnlocker]";
+    private logger: ILogger;
 
-    postDBLoad(container: DependencyContainer): void 
+    postDBLoad(container: DependencyContainer): void
     {
-        const logger = container.resolve<ILogger>("WinstonLogger");
+        this.logger = container.resolve<ILogger>("WinstonLogger");
         const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
-        databaseServer.getTables().settings.config.FramerateLimit.MaxFramerateLobbyLimit = Number(this.modConfig["framerate"]);
-        databaseServer.getTables().settings.config.FramerateLimit.MaxFramerateGameLimit = Number(this.modConfig["framerate"]);
-        logger.info("[FpsUnlocker] Applied framerate settings.");
+        const tables = databaseServer.getTables();
+
+        try {
+            container.resolve("FikaConfig")
+            this.isFikaInstalled = true;
+            this.logger.info(`${this.modName} Detected FIKA installation - forcing 144fps menu limit.`)
+        } catch (e) {
+            this.isFikaInstalled = false;
+        }
+
+        tables.settings.config.FramerateLimit.MaxFramerateLobbyLimit = this.isFikaInstalled
+            ? Number(this.modConfig["framerate"])
+            : Number(144)
+
+        tables.settings.config.FramerateLimit.MaxFramerateGameLimit = Number(this.modConfig["framerate"]);
+
+        this.logger.info("[FpsUnlocker] Applied framerate settings.");
     }
 }
-
-module.exports = { mod: new FpsUnlocker() };
+export const mod = new FpsUnlocker()
